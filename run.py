80c830b37478b5a9adadd48147eef996be0b0348
eSeF <coccogoggo@gmail.com>
2023-01-07 00:13:03 +0900

    add exp_code

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ import Envs
 import gym
 import random
 import numpy as np
+import sys
 
 from Libs import RNDfunc, PCTCfunc, EVALfunc
 
@@ -30,14 +31,15 @@ import matplotlib.pyplot as plt
 
 def Exp_Run(
     envname = 'myrogue-v0',
-    learn_timestep = 10000
+    learn_timestep = 5000000,
+    exp_code = 0
     ):
 
     print('START')
     ### 使用する環境の設定
     env = DummyVecEnv([lambda : gym.make(envname)])
     
-    log_dir = '../dummy/logs/01'
+    log_dir = '../dummy/logs/' + str(exp_code)
     os.makedirs(log_dir, exist_ok=True)
     ### Monitor の設定
     env = VecMonitor(env, log_dir)
@@ -53,7 +55,7 @@ def Exp_Run(
     model.learn(total_timesteps=learn_timestep)
 
 
-    model_dir = '../dummy/models/01'
+    model_dir = '../dummy/models/' + str(exp_code)
     os.makedirs(model_dir, exist_ok=True)
     model.save(model_dir)
 
@@ -63,7 +65,11 @@ def Exp_Run(
 ### コード実行部分
 
 def main():
-    Exp_Run()
+    args = sys.argv
+    num = 0
+    if len(args) >= 2:
+        num = int(args[1])
+    Exp_Run(exp_code=num)
 
 if __name__ == '__main__':
     main()
